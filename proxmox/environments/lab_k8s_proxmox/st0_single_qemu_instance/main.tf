0901921701cf368571ad2cfe6b2579bc4f44148b
chrisp0 <chrisp0@forgeops>
2025-02-28 10:10:48 +0000

    init v3

--- proxmox/environments/lab_k8s_proxmox/st0_single_qemu_instance/main.tf
+++ proxmox/environments/lab_k8s_proxmox/st0_single_qemu_instance/main.tf
@@ -13,14 +13,7 @@
 terraform {
   required_providers {
     proxmox = {
-      # Use these two lines to run a locally compiled version of the provider
-      #source = "registry.example.com/telmate/proxmox"
-      #version = ">=1.0.0"
-
-      # Normally we want the provider from the registry
       source = "Telmate/proxmox"
-      # We can specificy a specific version. If we do not, terraform will use
-      # the latest official release.
       #version = "=2.9.11"
       version = "=3.0.1-rc1"
     }
