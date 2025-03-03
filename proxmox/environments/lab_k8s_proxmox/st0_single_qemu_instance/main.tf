07ce51e23551a83ac177e214ecf15d2b39ca762f
chrisp0 <chrisp0@forgeops>
2025-03-03 22:01:07 +0000

    update providers

--- proxmox/environments/lab_k8s_proxmox/st0_single_qemu_instance/main.tf
+++ proxmox/environments/lab_k8s_proxmox/st0_single_qemu_instance/main.tf
@@ -1,30 +1,40 @@
-# terraform {
-#   required_version = ">= 1.1.0"
-#   required_providers {
-#     proxmox = {
-#       source  = "telmate/proxmox"
-#       version = ">= 2.9.5"
-#     }
-#   }
-# }
-# Proxmox provider configuration
-
-# Define the usual provider things
 terraform {
   required_providers {
     proxmox = {
-      source = "Telmate/proxmox"
-      #version = "=2.9.11"
-      version = "=3.0.1-rc1"
+      source  = "Telmate/proxmox"
+      version = "=3.0.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
     }
   }
-  #required_version = ">= 0.14"
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.10.0"
+
+  backend "s3" {
+    bucket         = "terraform-state-forgeops"
+    key            = "proxmox/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-state-lock"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_secretsmanager_secret" "proxmox" {
+  name = "terraform/proxmox"
+}
+
+data "aws_secretsmanager_secret_version" "proxmox" {
+  secret_id = data.aws_secretsmanager_secret.proxmox.id
 }
 
 provider "proxmox" {
-  pm_api_url          = "xxx"
-  pm_api_token_id     = "xxx" #tmp
-  pm_api_token_secret = "xxx" #tmp
-  pm_tls_insecure     = true  # Set to true if using a self-signed certificate
+  pm_api_url          = jsondecode(data.aws_secretsmanager_secret_version.proxmox.secret_string)["pm_api_url"]
+  pm_api_token_id     = jsondecode(data.aws_secretsmanager_secret_version.proxmox.secret_string)["pm_api_token_id"]
+  pm_api_token_secret = jsondecode(data.aws_secretsmanager_secret_version.proxmox.secret_string)["pm_api_token_secret"]
+  pm_tls_insecure     = jsondecode(data.aws_secretsmanager_secret_version.proxmox.secret_string)["pm_tls_insecure"]
 }
